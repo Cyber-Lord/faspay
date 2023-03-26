3fa2afbfcd289dc9745a6f6c6a1d9c3bf7ad3e3e
Cyber-Lord <Umaralfaruq02@gmail.com>
2023-03-27 00:18:38 +0100

    Added contact picking as account

--- lib/pages/transferpage.dart
+++ lib/pages/transferpage.dart
@@ -1,6 +1,8 @@
 import 'package:faspay/pages/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:pin_code_text_field/pin_code_text_field.dart';
+import 'package:contacts_service/contacts_service.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class BankTransferPage extends StatefulWidget {
   @override
@@ -9,14 +11,30 @@ class BankTransferPage extends StatefulWidget {
 
 class _BankTransferPageState extends State<BankTransferPage> {
   TextEditingController controller = TextEditingController(text: "");
+  final TextEditingController _accountNumberController =
+      TextEditingController();
+  final TextEditingController _amountController = TextEditingController();
+  final FocusNode _textFocusNode = FocusNode();
+
   String thisText = "";
   int pinLength = 4;
   bool hasError = false;
+
   late String errorMessage;
   late String selectedBank = 'Select Destination Bank';
   late String beneficiaryName = 'John Doe';
   double amount = 0.0;
   late String description = 'Salary';
+
+  Future<void> _selectContact() async {
+    PermissionStatus permissionStatus = await Permission.contacts.request();
+    if (permissionStatus == PermissionStatus.granted) {
+      Contact? contact = await ContactsService.openDeviceContactPicker();
+      if (contact != null && contact.phones!.isNotEmpty) {
+        _accountNumberController.text = contact.phones!.first.value!;
+      }
+    }
+  }
 
   List<String> banks = [
     'Select Destination Bank',
@@ -25,6 +43,18 @@ class _BankTransferPageState extends State<BankTransferPage> {
     'Kuda MFB',
     'Wema Bank',
   ];
+  @override
+  void initState() {
+    super.initState();
+    _textFocusNode.requestFocus();
+  }
+
+  @override
+  void dispose() {
+    _textFocusNode.dispose();
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,116 +76,156 @@ class _BankTransferPageState extends State<BankTransferPage> {
         ),
         title: Text('Bank Transfer'),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 0.08,
-              width: MediaQuery.of(context).size.width * 0.8,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.grey,
-                  width: 1.0,
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: DropdownButton<String>(
-                  value: selectedBank,
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    color: Colors.black87,
-                  ),
-                  items: banks.map((bank) {
-                    return DropdownMenuItem<String>(
-                      value: bank,
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * 0.8,
-                        child: Text(
-                          bank,
-                          style: TextStyle(
-                            fontSize: 16.0,
-                            color: Colors.blue.shade900,
-                          ),
-                        ),
-                      ),
-                    );
-                  }).toList(),
-                  hint: Text(
-                    'Select a Bank',
-                    style: TextStyle(
-                      fontSize: 16.0,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.08,
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  decoration: BoxDecoration(
+                    border: Border.all(
                       color: Colors.grey,
+                      width: 1.0,
                     ),
                   ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: DropdownButton<String>(
+                      value: selectedBank,
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.black87,
+                      ),
+                      items: banks.map((bank) {
+                        return DropdownMenuItem<String>(
+                          value: bank,
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.8,
+                            child: Text(
+                              bank,
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                color: Colors.blue.shade900,
+                              ),
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                      hint: Text(
+                        'Select a Bank',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          selectedBank = value!;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                Padding(
+                  padding: EdgeInsets.all(0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // ...
+
+                      Row(
+                        children: [
+                          Expanded(
+                            child: TextFormField(
+                              controller: _accountNumberController,
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                labelText: 'Account Number',
+                                border: OutlineInputBorder(),
+                              ),
+                            ),
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.contacts),
+                            onPressed: _selectContact,
+                          ),
+                        ],
+                      ),
+                      // ...
+                    ],
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                TextFormField(
                   onChanged: (value) {
                     setState(() {
-                      selectedBank = value!;
+                      beneficiaryName = value;
                     });
                   },
+                  decoration: InputDecoration(
+                    labelText: 'Account Name',
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.number,
                 ),
-              ),
+                SizedBox(height: 16.0),
+                TextFormField(
+                  controller: _amountController,
+                  onChanged: (value) {
+                    setState(() {
+                      amount = double.parse(value);
+                    });
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Amount',
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+                SizedBox(height: 16.0),
+                TextFormField(
+                  onChanged: (value) {
+                    setState(() {
+                      amount = double.parse(value);
+                    });
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Description',
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.text,
+                ),
+                SizedBox(height: 16.0),
+                Container(
+                  alignment: Alignment.bottomCenter,
+                  decoration: BoxDecoration(
+                    color: Colors.blue.shade900,
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  child: TextButton(
+                    onPressed: () {
+                      showPinDialog(context);
+                    },
+                    child: Text(
+                      'CONTINUE',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            SizedBox(height: 16.0),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Account Number',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.number,
-            ),
-            SizedBox(height: 16.0),
-            TextFormField(
-              onChanged: (value) {
-                setState(() {
-                  beneficiaryName = value;
-                });
-              },
-              decoration: InputDecoration(
-                labelText: 'Account Name',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.number,
-            ),
-            SizedBox(height: 16.0),
-            TextFormField(
-              onChanged: (value) {
-                setState(() {
-                  amount = double.parse(value);
-                });
-              },
-              decoration: InputDecoration(
-                labelText: 'Amount',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.number,
-            ),
-            SizedBox(height: 16.0),
-            TextFormField(
-              onChanged: (value) {
-                setState(() {
-                  amount = double.parse(value);
-                });
-              },
-              decoration: InputDecoration(
-                labelText: 'Description',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.text,
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.blue.shade900,
-              ),
-              child: Text('Transfer'),
-              onPressed: () {
-                showPinDialog(context);
-              },
-            ),
-          ],
+          ),
         ),
       ),
     );
